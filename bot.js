2e66ebc4b5e3c2b709592574d91c4c0335d4dbf0
b1354 <bayurizkyseptiansyah@gmail.com>
2022-05-16 16:23:26 +0700

    adding waifu command

--- bot.js
+++ bot.js
@@ -3,9 +3,9 @@ require('dotenv').config()
 const {Telegraf} = require('telegraf');
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-const command = require('./trigger/command')
-const botHelp = require('./trigger/botHelp')
-const indahHandler = require('./trigger/indahHandler');
+const command = require('./lib/command')
+const botHelp = require('./lib/botHelp')
+const indahHandler = require('./lib/indahHandler');
 
 bot.start( ctx => ctx.reply('Selamat datang ges, nama w ubay'))
 
@@ -13,10 +13,47 @@ bot.use(botHelp)
 bot.use(command.sayhi)
 bot.use(command.whoami)
 bot.use(indahHandler)
+bot.use(command.waifu)
 
 bot.on('sticker', (ctx) => {
   ctx.reply('hi maniezz')
   ctx.reply('😘')
+})
+
+bot.command('waifu', (ctx) => {
+  let waifuMessage = `chose your waifu`
+  bot.telegram.sendMessage(ctx.chat.id, waifuMessage, {
+    reply_markup: {
+      inline_keyboard: [
+        [{
+          text: "kato",
+          callback_data: "kato"
+        },
+        {
+          text: "utaha",
+          callback_data: "utaha"
+        }]
+      ]
+    }
+  })
+
+  bot.action('kato', (ctx) => {
+    ctx.deleteMessage()
+    ctx.replyWithMediaGroup([{
+      media: {source: "./res/kato.jpeg"},
+      caption: "waifu kamu kato, ini pfp nya",
+      type: "photo"
+    }])
+  })
+
+  bot.action('utaha', (ctx) => {
+    ctx.deleteMessage()
+    ctx.replyWithMediaGroup([{
+      media: {source: "./res/utaha.jpeg"},
+      caption: "waifu kamu Utaha senpai, ini pfp nya",
+      type: "photo"
+    }])
+  })
 })
 
 bot.launch()
