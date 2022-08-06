a171802c28d8809dc596ed6e36a0f7abd941698a
b1354 <bayurizkyseptiansyah@gmail.com>
2022-08-06 10:14:51 +0700

    - make bot ready for distribution - modify cat command from "miaw" to "meow" - fixing botHelp due to change with cat command - modify bot.js using express

--- bot.js
+++ bot.js
@@ -1,5 +1,9 @@
 require('dotenv').config()
 
+const express = require('express')
+const port = process.env.PORT || 3000
+const expressApp = express()
+
 const {Telegraf} = require('telegraf')
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
@@ -9,6 +13,14 @@ const serverInfo = require('./lib/serverInfo')
 const cat = require('./lib/cat')
 const quranAPI = require('./lib/quran')
 
+expressApp.get('/', (req, res) => {
+  res.send('bot running!!')
+})
+expressApp.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
+
+
 bot.start( ctx => ctx.reply('welcome to ubott_'))
 bot.help(botHelp)
 
@@ -16,14 +28,16 @@ bot.hears('oy', (ctx) => {
   ctx.reply('oyy, apa bang?')
 })
 
-bot.use(command.sayhi)
-bot.use(command.whoami)
-
-
 bot.command('quran', quranAPI.quran)
 bot.command('server', serverInfo)
 bot.command('replyme', command.replyMe)
-bot.command('miaw', cat)
+bot.command('sayhi', command.sayhi)
+bot.command('whoami', command.whoami)
+bot.command('meow', cat)
 
 bot.launch()
 console.log('bot running')
+
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
